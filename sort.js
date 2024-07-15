c25a4d3c0bd076cc775a7fea704f45e53b7493df
unkuseni <amudalatadeoye@gmail.com>
2024-07-16 00:33:09 +0100

    added sort

---
+++ sort.js
@@ -0,0 +1,17 @@
+function insertionSort(arr) {
+	const length = arr.length;
+
+	for (let i = 1; i < length; i++) {
+		let current = arr[i];
+		let j = i - 1;
+
+		while (j >= 0 && arr[j] > current) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+
+		arr[j + 1] = current;
+	}
+
+	return arr;
+}
