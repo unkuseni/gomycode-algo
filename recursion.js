0c564a2dda6c7b83a5b105d2f44191c8cfefae50
unkuseni <amudalatadeoye@gmail.com>
2024-07-16 01:23:48 +0100

    recursion

---
+++ recursion.js
@@ -0,0 +1,43 @@
+function isLeapYear(year) {
+	if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+		return `${year} is a leap year.`;
+	} else {
+		return `${year} is not a leap year.`;
+	}
+}
+
+
+function ticketPrice(age) {
+	let price;
+	if (age <= 12) {
+		price = 10;
+	} else if (age >= 13 && age <= 17) {
+		price = 15;
+	} else {
+		price = 20;
+	}
+	return `The ticket price for age ${age} is $${price}.`;
+}
+
+function fibonacci(n) {
+	if (n <= 1) {
+		return n;
+	} else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+function isPalindrome(str) {
+	// Remove spaces and convert to lowercase
+	str = str.toLowerCase().replace(/[\W_]/g, "");
+
+	if (str.length <= 1) {
+		return true;
+	}
+
+	if (str[0] !== str[str.length - 1]) {
+		return false;
+	} else {
+		return isPalindrome(str.substring(1, str.length - 1));
+	}
+}
